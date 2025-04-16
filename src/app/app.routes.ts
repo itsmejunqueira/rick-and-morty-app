e3ca4ed348f1d704a6caeeb18a9b4b7a77a3dcdb
Leticia C. Junqueira <67600443+itsmejunqueira@users.noreply.github.com>
2025-04-16 11:07:48 -0300

    fix estrutura

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { CharacterDetailsComponent } from './pages/characters/components/character-details/character-details.component';
+import { CharacterListComponent } from './pages/characters/components/character-list/character-list.component';
+import { EpisodeDetailsComponent } from './pages/episodes/components/episode-details/episode-details.component';
+import { EpisodeListComponent } from './pages/episodes/components/episode-list/episode-list.component';
+import { LocationListComponent } from './pages/location/components/location-list/location-list.component';
+import { LocationDetailsComponent } from './pages/location/components/location-details/location-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'character-details/:id', component: CharacterDetailsComponent },
+    { path: 'character-list', component: CharacterListComponent },
+    { path: 'episode-details/:id', component: EpisodeDetailsComponent },
+    { path: 'episode-list', component: EpisodeListComponent },
+    { path: 'location-details/:id', component: LocationDetailsComponent },
+    { path: 'location-list', component: LocationListComponent }
+]
